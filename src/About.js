4eaa84495047b2c81279e451aa2697ec46190c7c
Isaac <katsisaac50@gmail.com>
2018-05-29 01:13:13 +0200

    added files

---
+++ src/About.js
@@ -0,0 +1,12 @@
+
+import React, {Component} from 'react'
+import {BrowserRouter, Route, Link,  } from 'react-router-dom'
+
+export class About extends Component {
+    render() {
+      return (
+       <div>hello</div>
+      );
+    }
+  }
+  export default About;
