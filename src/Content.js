0bc80ecd713b139ab8e1402b4b716d6715d73a02
Isaac <katsisaac50@gmail.com>
2018-06-01 07:34:26 +0200

    redunced to only display company info.

--- src/Content.js
+++ src/Content.js
@@ -1,86 +1,20 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import ReactTable from 'react-table'
 import "react-table/react-table.css";
-import Home from './Home';
 
 // import $ from 'jquery';
 //import BootstrapTable from 'reactjs-bootstrap-table';
 
 class Content extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-          
-          userData: null
-        };
-        
-        {console.log(props)}
-    }
-    componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/users/')
-        .then(({ data }) => {
-                console.log(data);
-                this.setState(
-                    { userData: data }
-                );
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-            // $.ajax({
-            //     url:'http://localhost:3000/user.json',
-            //     dataType:'json',
-            //     cache: false,
-            //     success: function(data){
-            //       this.setState({ array: data });
-            //     }.bind(this),
-            //     error: function(xhr, status, err){
-            //       console.log(err);
-            //       alert(err);
-            //     }
-            //   });
-
-    }
+    
     render() {
-      if(!this.state.userData) {
-        return null;
-      }
+      {console.log(this.props.data)}
   
-  //Store data in uData
-        const uData  = this.state.userData;
-        console.log(uData);
        
         return(<div>
-          <Home grade={uData}/>
         <ReactTable
-          data={uData}
+          // data={uData}
           columns={[
-            {
-              Header: "User Details",
-              columns: [
-                {
-                  Header: "Name",
-                  accessor: "name"
-                },
-                {
-                  Header: "Username",
-                  id: "userName",
-                  accessor: d => d.username
-
-                },{
-                  Header: "Email",
-                  accessor: "email"
-                },{
-                  Header: "Phone",
-                  accessor: "phone"
-                },{
-                  Header: "Website",
-                  accessor: "website"
-                }
-              ]
-            },
             {
               Header: "Company Info",
               columns: [
@@ -115,36 +49,7 @@ class Content extends Component {
         />
        
       </div>)
-//         return (
-//             <div>
-//                 <div></div>
-//                 <h3>User Details</h3>
-//                 <ul >
-//                     {this.renderCompany()}
-//                 </ul>
 
-//             </div>
-//         );
-//     }
-//     renderCompany() {
-//         const compData = this.state.array;
-//         return compData.map(userData => {
-//             console.log(userData);
-//             return (
-//                 <div>
-//                     <tr>
-//                         {/* <td>{company.username}</td> */}
-                       
-//                         <td>{userData.name}</td>
-//                     </tr>
-//                     {/* <ul>
-//                         <li className="list-group-item" key={company.name}>
-//                             {company.name}
-//                         </li>
-//                     </ul> */}
-//                 </div>
-//             );
-//         });
     }
 
 
