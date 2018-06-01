d4844b6c74b56e98d7f34bb56fd198b18e3578ae
Isaac <katsisaac50@gmail.com>
2018-06-01 07:32:33 +0200

    transfered the getUserData function in this component.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import './App.css';
 import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
 //curl braces are used when you import from export by name.
 import { About } from './About';
 import Home from './Home';
 import Content from './Content';
-
+import axios from 'axios';
 
 
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      userData:{}
+    }
+  }
+  getUserData(){
+    axios.get('https://jsonplaceholder.typicode.com/users/')
+    .then(({ data }) => {
+            console.log(data);
+            this.setState(
+                { userData: data }
+            );
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    
+  }
+  componentDidMount() {
+    this.getUserData()
+
+}
+
   render() {
+    
     return (
       <div>
+        {console.log(this.state.userData)}
         <nav >
           <ul >
 
@@ -22,10 +48,13 @@ class App extends Component {
 
           </ul>
         </nav>
+        <Home data={this.state.userData} />
+        <Content data={this.state.userData} />
+
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/About" component={About} />
-          <Route path="/content" component={Content} />
+          <Route path="/Content" component={Content} />
         </Switch>
       </div>
     );
