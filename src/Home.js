8d57cf284663f914d91487f84bc068de1f1ed4fd
Isaac <katsisaac50@gmail.com>
2018-06-01 07:38:06 +0200

    reduced to display only User details.

--- src/Home.js
+++ src/Home.js
@@ -3,22 +3,17 @@ import ReactTable from 'react-table'
 import Content from './Content';
 
 class Home extends Component{
-    constructor(props) {
-        super(props);
-        {console.log(props)}
-        this.state = {
-          
-            fetchedData: this.props,
-          };
-    }
+
     render(){
-        const uData2=this.props.grade;
+      
+        const uData2=this.props.data;
         console.log(uData2)
         return (
             <div> 
+
                 Hello you are Home!!
-                <ReactTable
-          data={uData2}
+                <ReactTable 
+          // data={uData2}
           columns={[
             {
               Header: "User Details",
@@ -41,34 +36,6 @@ class Home extends Component{
                 },{
                   Header: "Website",
                   accessor: "website"
-                }
-              ]
-            },
-            {
-              Header: "Company Info",
-              columns: [
-                {
-                  Header: "Name",
-                  accessor: "company.name"
-                },
-                {
-                  Header: "Service",
-                  accessor: "company.bs"
-                }
-              ]
-            },
-            {
-              Header: 'Address',
-              columns: [
-                {
-                  Header: "City",
-                  accessor: "address.city"
-                },{
-                  Header: "Streat",
-                  accessor: "address.street"
-                },{
-                  Header: "Suite",
-                  accessor: "address.suite"
                 }
               ]
             }
